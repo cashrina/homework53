5e361af2733623f5c819f1bdf6506b99773db60b
cashrina <khripunova.irina.24@gmail.com>
2024-05-23 14:29:03 +0600

    Описала внешний вид формы

---
+++ my-react-app/src/AddTaskForm/AddTaskForm.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddTaskForm = () => {
+
+    const onFormSubmit = (event:React.FormEvent) => {
+        event.preventDefault();
+    }
+    return (
+        <form className="add-task-form" onSubmit={onFormSubmit}>
+            <h6>Create Task</h6>
+            <label>Add:
+                <input placeholder="Add a task"  type="text" />
+            </label>
+            <button type="submit" >Add</button>
+        </form>
+    );
+};
+
+export default AddTaskForm;
